476f2da26fa561cbd823291a92d1330f70da72b3
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-02-10 19:19:29 -0600

    first three cases solved and working for correct return

--- supermarket_time_to_queue.js
+++ supermarket_time_to_queue.js
@@ -24,11 +24,15 @@
 
 // REFACTORED FIRST PASS
 function queueTime(customers, n) {
-  if (customers.length == 0) return 0
+  // takes care of the case where array is empty
+  if (customers.length === 0) return 0;
 
+  // 
   // ... is a short way to write .apply, should not be used for very large arrays.
-  if (n >= customers.length) return Math.max(...customers)
-
+  if (n >= customers.length) return Math.max(...customers);
+  
+  // takes care of the case where there are NOT multiple threads
+  if (n === 1) return customers.reduce(function(a,b) { return a+b}, 0);
 }
 
 // First Pass
@@ -55,3 +59,13 @@ queueTime([1,2,3,4,5], 100);  // returns 5
 queueTime([1,2,3,4], 1);      // returns 10
 
 queueTime([2,2,3,3,4,4], 2);  // returns 9
+
+//queueTime([3,18,12,2,5,6,8,13,5,3,1,8], 3); // returns... 31
+
+// 16,18,12
+// 16,18,20
+// 29,18,20
+// 29,23,20
+// 29,23,23
+// 29,24,23
+// 29,24,31
