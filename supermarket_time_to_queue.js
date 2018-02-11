dc84dce118ba41389b2896a96299cf69bec52169
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-02-10 18:38:20 -0600

    initial commit updated readme with challenge description and added js file and started driver and pseudocode

---
+++ supermarket_time_to_queue.js
@@ -0,0 +1,20 @@
+// PSEUDOCODE
+// if customer array is empty return 0
+
+
+
+// CODE
+function queueTime(customers, n) {
+  //TODO
+  if (customers.length == 0) return 0;
+}
+
+
+
+// DRIVER CODE
+
+
+queueTime([], 1);             // returns 0
+queueTime([1,2,3,4], 1);      // returns 10
+queueTime([2,2,3,3,4,4], 2);  // returns 9
+queueTime([1,2,3,4,5], 100);  // returns 5
